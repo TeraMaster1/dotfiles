7b2b5b1a7c45096c0454f2312bc1447a6cfb3b5d
TeraMaster1 <teraboykisser@proton.me>
2025-08-29 17:27:24 +0200

    bytte till nvchad för att åtgärda ett par fel gällande lsp

--- nvim/.config/nvim/lua/misu/lsp/clangd.lua
+++
@@ -1,7 +0,0 @@
-vim.lsp.config['clangd'] = {
-  cmd = { 'clangd' },
-  root_markers = { '.clangd', 'compile_commands.json' },
-  filetypes = { 'c', 'cc', 'cpp' },
-}
-
-vim.lsp.enable('clangd')
--- nvim/.config/nvim/lua/misu/lsp/init.lua
+++
@@ -1,2 +0,0 @@
-require("misu.lsp.luals")
-require("misu.lsp.clangd")
