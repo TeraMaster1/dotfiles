a4974647b60288617fc8288feedb3a11ae7fc9ba
TeraMaster1 <teraboykisser@proton.me>
2025-11-06 13:06:07 +0100

    gjorde så att ett felmeddelande inte längre dyker upp

--- nvim/.config/nvim/lua/misu/config/lsp-completion.lua
+++ nvim/.config/nvim/lua/misu/config/lsp-completion.lua
@@ -1,7 +1,7 @@
 vim.cmd[[set completeopt+=menuone,noselect,popup]]
 vim.lsp.start({
   name = 'ts_ls',
-  cmd = …,
+  cmd = {'typescript-language-server', '--stdio'},
   on_attach = function(client, bufnr)
     vim.lsp.completion.enable(true, client.id, bufnr, {
       autotrigger = true,
